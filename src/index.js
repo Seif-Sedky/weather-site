660d729c1aa49c7256f5ccca7503e5eb9fac9719
Seif Sedky <Seif.alaa1231@gmail.com>
2025-07-27 16:55:25 +0300

    Add display utils

--- src/index.js
+++ src/index.js
@@ -1,1 +1,40 @@
 import './index.css';
+import { getData } from './utils/api';
+import { displayContent } from './utils/content-display';
+import { displayError } from './utils/error-display';
+
+function DOM() {
+    const input = document.querySelector('.search-bar');
+    const summary = document.querySelector('.summary');
+    const degree = document.querySelector('.degree');
+    const feel = document.querySelector('.feel');
+    const wind = document.querySelector('.wind');
+    const humidity = document.querySelector('.humidity');
+    const days = document.querySelectorAll('.day');
+    const minMaxTemps = document.querySelectorAll('.min-max-temp');
+
+    return { input, summary, degree, feel, wind, humidity, days, minMaxTemps };
+}
+
+
+function initializeEventListener() {
+    let dom = DOM();
+
+    dom.input.addEventListener('keydown', function (event) {
+        if (event.key === 'Enter' && dom.input.value) {
+            const value = dom.input.value;
+            let p = new Promise((resolve, reject) => {
+                getData(value)
+                    .then(data => resolve(data))
+                    .catch(error => reject(error));
+            }).then(response => displayContent(response, dom)) 
+                .catch(displayError(error, dom));
+
+        }
+    });
+}
+
+
+
+
+initializeEventListener();
---
+++ src/utils/content-display.js
@@ -0,0 +1,3 @@
+export function displayContent(data,dom){
+
+}
